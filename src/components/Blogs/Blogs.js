d69facfd4207842d60acb73e6cb1b12fa25e283f
Soyeb <tssoyeb@gmail.com>
2022-04-04 09:49:56 +0600

    create component

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            it's blogs
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            its dashboard
+        </div>
+    );
+};
+
+export default Dashboard;
