f73177f0f1f7883820bb40d8e5eb939455ddaf82
Soyeb <tssoyeb@gmail.com>
2022-04-04 10:57:15 +0600

    NotFound done

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import images from '../images/notfound.jpg'
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div>
+           <img src={images} alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
